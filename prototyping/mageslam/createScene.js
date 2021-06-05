0011826aafe68adb505eb212286fc9acb6b7ab9f
syntheticmagus <33846034+syntheticmagus@users.noreply.github.com>
2021-06-04 20:48:46 -0700

    Added perf metric.

--- prototyping/mageslam/createScene.js
+++ prototyping/mageslam/createScene.js
@@ -8,31 +8,43 @@ var createScene = async function (engine, canvas) {
     var advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
 
     var rect1 = new BABYLON.GUI.Rectangle();
-    rect1.width = "40px";
+    rect1.width = "80px";
     rect1.height = "40px";
     rect1.cornerRadius = 20;
-    rect1.color = "orange";
+    rect1.color = "white";
     rect1.thickness = 4;
     rect1.background = "white";
     rect1.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
     rect1.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_TOP;
     advancedTexture.addControl(rect1);
 
+    const txt = new BABYLON.GUI.TextBlock("fpsText", "0 FPS");
+    rect1.addControl(txt);
+
     const createVideoTexture = async function() {
         //canvas.requestFullscreen();
 
         var ANote0VideoVidTex = await BABYLON.VideoTexture.CreateFromWebCamAsync(scene, {facingMode: "environment"});
+        ANote0VideoVidTex.vScale = -1;
         
         var layer = new BABYLON.Layer('', undefined, scene, true);
         layer.texture = ANote0VideoVidTex;
         
         const slam = await Microsoft.MageSlam.CreateAsync(ANote0VideoVidTex);
+        let prior = Date.now();
+        let interval = 0;
         slam.onTrackingUpdatedObservable.add((tracked) => {
             if (tracked) {
                 rect1.background = "green";
             } else {
                 rect1.background = "red";
             }
+
+            const now = Date.now();
+            interval = 0.9 * interval + 0.1 * Math.round(1000 / (now - prior));
+            prior = now;
+
+            txt.text = Math.round(interval) + " FPS";
         });
         await BABYLON.Tools.DelayAsync(1000);
         slam.startTracking();
