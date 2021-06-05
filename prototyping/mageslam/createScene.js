77663b6abb13f620eddfc7983cc724ab9b3817f0
syntheticmagus <33846034+syntheticmagus@users.noreply.github.com>
2021-06-04 20:21:20 -0700

    Attempting to achieve first interactive test.

--- prototyping/mageslam/createScene.js
+++ prototyping/mageslam/createScene.js
@@ -4,43 +4,51 @@ var createScene = async function (engine, canvas) {
 
     // This creates and positions a free camera (non-mesh)
     var camera = new BABYLON.ArcRotateCamera("arcR", -Math.PI/2, Math.PI/2, 15, BABYLON.Vector3.Zero(), scene);
+    
+    var advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
 
-    // This attaches the camera to the canvas
-    camera.attachControl(canvas, true);
+    var rect1 = new BABYLON.GUI.Rectangle();
+    rect1.width = "40px";
+    rect1.height = "40px";
+    rect1.cornerRadius = 20;
+    rect1.color = "orange";
+    rect1.thickness = 4;
+    rect1.background = "white";
+    rect1.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
+    rect1.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_TOP;
+    advancedTexture.addControl(rect1);
 
-	var planeOpts = {
-			height: 5.4762, 
-			width: 7.3967, 
-			sideOrientation: BABYLON.Mesh.DOUBLESIDE
-	};
-	var ANote0Video = BABYLON.MeshBuilder.CreatePlane("plane", planeOpts, scene);
-	var vidPos = (new BABYLON.Vector3(0,0,0.1))
-    ANote0Video.position = vidPos;
+    const createVideoTexture = async function() {
+        //canvas.requestFullscreen();
 
-    BABYLON.Tools.DelayAsync(5000).then(async function() {
-        var ANote0VideoMat = new BABYLON.StandardMaterial("m", scene);
-        var ANote0VideoVidTex = new BABYLON.VideoTexture("vidtex","bananas.mp4", scene);
-        ANote0VideoVidTex.video.playbackRate = 0.25;
-        ANote0VideoVidTex.video.loop = false;
-        ANote0VideoMat.diffuseTexture = ANote0VideoVidTex;
-        ANote0VideoMat.roughness = 1;
-        ANote0VideoMat.emissiveColor = new BABYLON.Color3.White();
-        ANote0Video.material = ANote0VideoMat;
-        scene.onPointerObservable.add(function(evt){
-                if(evt.pickInfo.pickedMesh === ANote0Video){
-                    //console.log("picked");
-                        if(ANote0VideoVidTex.video.paused)
-                            ANote0VideoVidTex.video.play();
-                        else
-                            ANote0VideoVidTex.video.pause();
-                        console.log(ANote0VideoVidTex.video.paused?"paused":"playing");
-                }
-        }, BABYLON.PointerEventTypes.POINTERPICK);
-
+        var ANote0VideoVidTex = await BABYLON.VideoTexture.CreateFromWebCamAsync(scene, {facingMode: "environment"});
+        
+        var layer = new BABYLON.Layer('', undefined, scene, true);
+        layer.texture = ANote0VideoVidTex;
+        
         const slam = await Microsoft.MageSlam.CreateAsync(ANote0VideoVidTex);
+        slam.onTrackingUpdatedObservable.add((tracked) => {
+            if (tracked) {
+                rect1.background = "green";
+            } else {
+                rect1.background = "red";
+            }
+        });
         await BABYLON.Tools.DelayAsync(1000);
         slam.startTracking();
+    };
+
+    var button1 = BABYLON.GUI.Button.CreateSimpleButton("but1", "Click Me");
+    button1.width = "150px"
+    button1.height = "40px";
+    button1.color = "white";
+    button1.cornerRadius = 20;
+    button1.background = "green";
+    button1.onPointerUpObservable.add(function() {
+        button1.dispose();
+        createVideoTexture();
     });
+    advancedTexture.addControl(button1);
 
     //console.log(ANote0Video);
     return scene;
